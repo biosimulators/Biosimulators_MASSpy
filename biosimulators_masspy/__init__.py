1e3dbd8b1529aa9b2a13de1bf33f0aebb0052286
Jonathan Karr <jonrkarr@gmail.com>
2021-09-02 23:04:25 -0400

    feat: updating to biosimulators-utils 0.1.116

--- biosimulators_masspy/__init__.py
+++ biosimulators_masspy/__init__.py
@@ -1,11 +1,13 @@
 from ._version import __version__
-from .core import exec_sed_task, exec_sedml_docs_in_combine_archive  # noqa: F401
+from .core import exec_sed_task, preprocess_sed_task, exec_sed_doc, exec_sedml_docs_in_combine_archive  # noqa: F401
 import mass
 
 __all__ = [
     '__version__',
     'get_simulator_version',
     'exec_sed_task',
+    'preprocess_sed_task',
+    'exec_sed_doc',
     'exec_sedml_docs_in_combine_archive',
 ]
 
